b7e664cb79314f961e4802a340d418eb9ed63924
curcioriccardo@gmail.com <curcioriccardo@gmail.com>
2021-04-28 10:02:54 +0200

    super case converter 2000

--- src/gateway/__init__.py
+++ src/gateway/__init__.py
@@ -1,14 +1,17 @@
+from converter import case
+
 class Gateway:
 
     @staticmethod
     def get(gatewayPath):
         try:
             modulePath = f'gateways.{gatewayPath}'
-            modName = (modulePath.split('.')[-1]).capitalize()
+            modName = case.pascal(string=modulePath.split('.')[-1], replaceSeparator='-')
+            modName = case.pascal(string=modName, replaceSeparator='_')
             gateway = Gateway.load_module(modulePath, modName)
             return getattr(gateway, modName)()
         except Exception as e:
-            print(f"gateway error {gatewayPath}")
+            print(f"gateway error {gatewayPath} {e}")
 
     @staticmethod
     def load_module(modPath, modName):
